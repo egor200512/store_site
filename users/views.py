9b7d03c8a557c7ba8fc2cf6a812ff5c40a0eebf6
Egor <volodinea2005@gmail.com>
2024-12-17 18:38:32 +0300

    login and registration done

--- users/views.py
+++ users/views.py
@@ -1,7 +1,41 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.urls import reverse
+
+from users.forms import UserLoginForm, UserRegistrationForm
+
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('home'))
+    else:
+        form = UserLoginForm()
+
+    context = {
+        'form': UserLoginForm
+    }
+    return render(request, 'users/login.html', context=context)
+
 
 def register(request):
-    return render(request, 'users/register.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'users/register.html', context=context)
+
