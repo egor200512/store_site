86b17d4b1a631ac9fde129ecc3bcf7feee76a344
Egor <volodinea2005@gmail.com>
2025-01-15 15:19:28 +0300

    pgsql

--- store/settings.py
+++ store/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 
 import django.core.mail.backends.console
 
+import psycopg2
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -82,10 +84,15 @@ WSGI_APPLICATION = 'store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'store',
+        'USER': 'postgres',
+        'PASSWORD': '25082006',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
