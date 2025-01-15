ae7a2147f831f0839f41da097ff32ca676aaa57c
Egor <volodinea2005@gmail.com>
2025-01-15 13:38:23 +0300

    context basket

--- users/views.py
+++ users/views.py
@@ -37,11 +37,6 @@ class UserProfileView(TitleMixin, UpdateView):
     form_class = UserProfileForm
     title = 'Профиль'
 
-    def get_context_data(self, **kwargs):
-        context = super(UserProfileView, self).get_context_data()
-        context['baskets'] = Basket.objects.filter(user=self.request.user)
-        return context
-
     def get_success_url(self):
         return reverse_lazy('users:profile', args=(self.object.id,))
 
